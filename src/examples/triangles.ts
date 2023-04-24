f818ef54065796b92be7c3094e383b9e42e25219
kkokkojeong <comgraphics11@gmail.com>
2023-04-24 17:06:59 +0900

    create triangle (ex1)

--- src/examples/triangles.ts
+++ src/examples/triangles.ts
@@ -8,19 +8,37 @@ type ExTriangleOptions = {
 // shader codes
 //
 const vertexShader = `
+    struct Output {
+        @builtin(position) Position : vec4<f32>,
+        @location(0) vColor : vec4<f32>,
+    };
+
     @vertex
-    fn main(@builtin(vertex_index) VertexIndex: u32) -> @builtin(position) vec4<f32> {
+    fn main(@builtin(vertex_index) VertexIndex: u32) -> Output {
         var pos = array<vec2<f32>, 3>(
             vec2<f32>(0.0, 0.5),
             vec2<f32>(-0.5, -0.5),
-            vec2<f32>(0.5, -0.5));
-        return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+            vec2<f32>(0.5, -0.5)
+        );
+        var color = array<vec3<f32>, 3>(
+            vec3<f32>(1.0, 0.0, 0.0),
+            vec3<f32>(0.0, 1.0, 0.0),
+            vec3<f32>(0.0, 0.0, 1.0)
+        );
+        
+
+        var output: Output;
+
+        output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+        output.vColor = vec4<f32>(color[VertexIndex], 1.0);
+
+        return output;
     }
 `;
 const fragmentShader = `
     @fragment
-    fn main() -> @location(0) vec4<f32> {
-        return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+    fn main(@location(0) vColor: vec4<f32>) -> @location(0) vec4<f32> {
+        return vColor;
     }
 `;
 
