de2bba35d4ecf017581c6f8f230e276b63e915d8
kkokkojeong <comgraphics11@gmail.com>
2023-04-28 15:59:24 +0900

    create cube (ex3)

---
+++ src/examples/cube.ts
@@ -0,0 +1,266 @@
+import { mat4 } from "gl-matrix";
+import {vertices, colors} from "../data/cube-vertices-colors";
+import Transform from "../transform/transform";
+import WebGPUHelper from "../util/WebGPUHelper";
+
+//
+// shader codes
+//
+const vertexShader = `
+    struct Output {
+        @builtin(position) Position : vec4<f32>,
+        @location(0) vColor : vec4<f32>,
+    };
+
+    @binding(0) @group(0) var<uniform> modelViewProj : mat4x4<f32>;
+
+    @vertex
+    fn main(@location(0) pos: vec4<f32>, @location(1) color: vec4<f32>) -> Output {
+        var output: Output;
+
+        output.Position = modelViewProj * pos;
+        output.vColor = color;
+
+        return output;
+    }
+`;
+const fragmentShader = `
+    @fragment
+    fn main(@location(0) vColor: vec4<f32>) -> @location(0) vec4<f32> {
+        return vColor;
+    }
+`;
+
+class ExCube {
+    private _canvas: HTMLCanvasElement;
+
+    // WebGPU
+    private _adapter: GPUAdapter | null = null;
+    private _device: GPUDevice | null = null;
+    private _context: GPUCanvasContext | null = null;
+    private _pipeline: GPURenderPipeline | null = null;
+
+    private _vertexBuffer: GPUBuffer | null = null;
+    private _colorBuffer: GPUBuffer | null = null;
+
+    private _uniformBuffer: GPUBuffer | null = null;
+    private _uniformBindGroup: GPUBindGroup | null = null;
+
+    private _initialized: boolean = false;
+
+    constructor(canvasId: string) {
+        this._canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+        console.log(vertices, colors);
+    }
+
+    public async render() {
+        if (!this._initialized) await this.initialize();
+        
+        const device = this._device as GPUDevice;
+        const context = this._context as GPUCanvasContext;
+        const pipeline = this._pipeline as GPURenderPipeline;
+
+        const commandEncoder = device.createCommandEncoder();
+
+        const textureView = context.getCurrentTexture().createView();
+        const depthTexture = device.createTexture({
+            size: [this._canvas.width, this._canvas.height, 1],
+            format: "depth24plus",
+            usage: GPUTextureUsage.RENDER_ATTACHMENT
+        });
+
+        const renderPass = commandEncoder.beginRenderPass({
+            colorAttachments: [{
+                view: textureView,
+                clearValue: { r: 0.8, g: 0.8, b: 0.8, a: 1.0 }, // background color
+                loadOp: 'clear',
+                storeOp: 'store'
+            }],
+            // for 3d, set depth buffer
+            depthStencilAttachment: {
+                view: depthTexture.createView(),
+                depthClearValue: 1.0,
+                depthLoadOp: 'clear',
+                depthStoreOp: "store",
+                /*stencilClearValue: 0,
+                stencilLoadValue: 0,
+                stencilLoadOp: 'clear',
+                stencilStoreOp: "store"*/
+            }
+        });
+
+        const numOfVertices = vertices.length / 3; // 18 / 3(x,y,z)
+        const aspectRatio = this._canvas.width / this._canvas.height;
+
+        // uniforms
+        const mvp = mat4.create();
+
+        const m = Transform.getModelMatrix();
+        
+        // test model matrix...
+        // const m = Transform.getModelMatrix([0, 0, 0], [0, 0, 0], [0.5, 0.5, 0.5]);
+
+        const p = Transform.getProjectionMatrix(60, aspectRatio, 0.1, 100);
+        // const v = Transform.getCameraMatrix([0, 0, 3], [0, 0, 0], [0, 1, 0]);
+        const v = Transform.getCameraMatrix([2, 2, 4], [0, 0, 0], [0, 1, 0]);
+
+        console.log(aspectRatio, this._canvas.width , this._canvas.height);
+        
+        mat4.multiply(mvp, p, v);
+        mat4.multiply(mvp, mvp, m);
+
+        this.createUniformBufferBinding();
+
+        console.log(this._uniformBuffer, mvp, m);
+
+        device.queue.writeBuffer(this._uniformBuffer as GPUBuffer, 0, mvp as ArrayBuffer);
+
+        renderPass.setPipeline(pipeline);
+
+        // draw vertices using buffer
+        renderPass.setVertexBuffer(0, this._vertexBuffer);
+        renderPass.setVertexBuffer(1, this._colorBuffer);
+
+        // binding unifrom
+        renderPass.setBindGroup(0, this._uniformBindGroup);
+
+        renderPass.draw(numOfVertices);
+
+        renderPass.end();
+
+        device.queue.submit([commandEncoder.finish()]);
+    }
+
+    private async initialize() {
+        if (this._initialized) return;
+
+        const adapter = await navigator.gpu.requestAdapter();
+        if (!adapter) {
+            throw new Error("WebGPURenderer: Unable to create WebGPU adapter.");
+        }
+
+        const device = await adapter.requestDevice();
+
+        const context = this._canvas.getContext("webgpu");
+        
+        this._adapter = adapter;
+        this._device = device;
+        this._context = context;
+
+        this.configureContext();
+
+        this.createVerticesAndColors();
+
+        this.createRenderPipeline();
+
+        this._initialized = true;
+    }
+
+    private configureContext() {
+        const device = this._device;
+        const context = this._context;
+        if (!device || !context) {
+            return;
+        }
+
+        context.configure({
+            device,
+            format: "bgra8unorm",
+            alphaMode: "opaque"
+        });
+    }
+
+    private createVerticesAndColors() {
+        const device = this._device;
+        if (!device) {
+            return;
+        }
+
+        this._vertexBuffer = WebGPUHelper.createBuffer(device, vertices, GPUBufferUsage.VERTEX);
+        this._colorBuffer = WebGPUHelper.createBuffer(device, colors, GPUBufferUsage.VERTEX);
+    }
+
+    private createUniformBufferBinding() {
+        const device = this._device;
+        const pipeline = this._pipeline;
+        if (!device || !pipeline) {
+            return;
+        }
+
+        this._uniformBuffer = device.createBuffer({
+            size: 64,
+            usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+        });
+
+        this._uniformBindGroup = device.createBindGroup({
+            layout: pipeline.getBindGroupLayout(0),
+            entries: [
+                {
+                    binding: 0,
+                    resource: {
+                        buffer: this._uniformBuffer,
+                        offset: 0,
+                        size: 64
+                    }
+                }
+            ]
+        });
+    }
+
+    private createRenderPipeline() {
+        const device = this._device;
+        if (!device) {
+            return;
+        }
+
+        this._pipeline = device.createRenderPipeline({
+            layout: "auto",
+            vertex: {
+                module: device.createShaderModule({
+                    code: vertexShader
+                }),
+                entryPoint: "main",
+                // if use GPU buffers, the buffers property is a must
+                buffers: [
+                    {
+                        arrayStride: 12,
+                        attributes: [{
+                            shaderLocation: 0,
+                            format: "float32x3",
+                            offset: 0
+                        }]
+                    },
+                    {
+                        arrayStride: 12,
+                        attributes: [{
+                            shaderLocation: 1,
+                            format: "float32x3",
+                            offset: 0
+                        }]
+                    },
+                ]
+            },
+            fragment: {
+                module: device.createShaderModule({
+                    code: fragmentShader
+                }),
+                entryPoint: "main",
+                targets: [{
+                    format: "bgra8unorm"
+                }]
+            },
+            primitive: {
+                topology: "triangle-list", // "triangle-strip",
+                // stripIndexFormat: undefined, // "uint32"
+                cullMode: "back", // back-culling
+            },
+            depthStencil: {
+                format: "depth24plus",
+                depthWriteEnabled: true,
+                depthCompare: "less"
+            }
+        });
+    }
+}
+
+export default ExCube;
--- src/transform/transform.ts
+++ src/transform/transform.ts
@@ -1,20 +1,49 @@
-import { vec3, mat4 } from "gl-matrix";
+import { vec3, mat4, glMatrix } from "gl-matrix";
+
+
+const toRadian = glMatrix.toRadian;
 
 class Transform {
+    public static getModelMatrix(translation: vec3 = [0, 0, 0], rotation: vec3 = [0, 0, 0], scaling: vec3 = [1, 1, 1]) {
+        const rx = mat4.create();
+        const ry = mat4.create();
+        const rz = mat4.create();
+
+        const t = mat4.create();
+        const s = mat4.create();
+
+        const m = mat4.create();
+
+        mat4.fromXRotation(rx, toRadian(rotation[0]));
+        mat4.fromXRotation(ry, toRadian(rotation[1]));
+        mat4.fromXRotation(rz, toRadian(rotation[2]));
+
+        mat4.fromTranslation(t, translation);
+        mat4.fromScaling(s, scaling);
+
+        // first multiply scaling certainly!
+        mat4.multiply(m, rx, s);
+        mat4.multiply(m, ry, m);
+        mat4.multiply(m, rz, m);
+        mat4.multiply(m, t, m);
+
+        return m;
+    }
+
     // like glPerspective
     public static getProjectionMatrix(fovy: number = 45, aspect: number = 1.0, near: number = 0.1, far: number = 100) {
         const p = mat4.create();
 
-        mat4.perspective(p, fovy, aspect, near, far);
+        mat4.perspective(p, toRadian(fovy), aspect, near, far);
 
         return p;
     }
 
     // like gluLookAt 
-    public static getCameraMatrix(pos: vec3, up: vec3 = [0, 1, 0], at: vec3 = [0, 0, 0]) {
+    public static getCameraMatrix(pos: vec3, at: vec3 = [0, 0, 0], up: vec3 = [0, 1, 0]) {
         const c = mat4.create();
 
-        mat4.lookAt(c, pos, up, at);
+        mat4.lookAt(c, pos, at, up);
 
         return c;
     }
