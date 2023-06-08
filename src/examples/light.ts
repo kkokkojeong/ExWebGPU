63559058a5552a77a88ef6822854ec962f85a90b
kkokkojeong <comgraphics11@gmail.com>
2023-06-08 18:10:10 +0900

    add light example

--- src/examples/light.ts
+++ src/examples/light.ts
@@ -1,7 +1,64 @@
 
+import {vertices, colors, indices} from "../data/cube-vertices-colors-indices";
+
 class ExLight {
+
+    private _canvas: HTMLCanvasElement;
+
+    private _adapter: GPUAdapter | null;
+    private _device: GPUDevice | null;
+    private _context: GPUCanvasContext | null;
+    private _format: GPUTextureFormat
+
+    private _initialized: boolean = false;
+
     constructor(id: string) {
-        console.log("canvas id", id);
+        this._canvas = document.getElementById(id) as HTMLCanvasElement;
+
+        console.log(vertices, vertices.length / 3);
+    }
+
+    public async render() {
+        const start = performance.now();
+
+        if (!this._initialized) await this.initialize();
+
+        
+        const end = performance.now();
+        const elapsedTime = (end - start) / 1000;
+
+        console.info(`elapsedTime: ${elapsedTime}ms`);
+    }
+
+    private async initialize() {
+        if (this._initialized) return;
+
+        const adapter = await navigator.gpu.requestAdapter();
+        if (!adapter) {
+            return;
+        }
+        const device = await adapter.requestDevice();
+
+        const context = this._canvas.getContext("webgpu");
+        const format = navigator.gpu.getPreferredCanvasFormat();
+        context?.configure({
+            device,
+            format,
+            alphaMode: 'opaque'
+        });
+
+        this._adapter = adapter;
+        this._device = device;
+        this._context = context;
+        this._format = format;
+
+        // create buffer, color
+
+        // render pipeline
+
+
+
+        this._initialized = true;
     }
 }
 
